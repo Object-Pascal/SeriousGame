a33fddee2b9f91236558190bd50b8250a509901c
Aanastasia Hellemons <amchelle@avans.nl>
2022-06-08 13:53:58 +0100

    [Added] method to extract the gameName from the input

--- Assets/Scripts/UIScripts/ButtonScrips/NewGameUIManager.cs
+++ Assets/Scripts/UIScripts/ButtonScrips/NewGameUIManager.cs
@@ -7,6 +7,10 @@ public class NewGameUIManager : MonoBehaviour
 {
     public TextMeshProUGUI header;
     public TextMeshProUGUI gameName;
+    public TextMeshProUGUI gameNamePlaceholder;
+    public TextMeshProUGUI gameNameInput;
+
+    private string gameNameString;
 
     public void ChangeHeaderText(string text)
     {
@@ -16,5 +20,15 @@ public class NewGameUIManager : MonoBehaviour
     public void ChangeGameNameText(string text)
     {
         gameName.text = text;
+    }
+
+    public void ChangeGameNamePlaceholderText(string text)
+    {
+        gameNamePlaceholder.text = text;
+    }
+
+    public void GetGameNameInputText()
+    {
+        gameNameString = gameNameInput.text;
     }
 }
