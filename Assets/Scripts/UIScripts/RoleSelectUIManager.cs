c254782bab5f3fd8ea93752932dfd9d22c23eaf5
Aanastasia Hellemons <amchelle@avans.nl>
2022-06-10 13:57:20 +0100

    [Added] Header component with hook

--- Assets/Scripts/UIScripts/RoleSelectUIManager.cs
+++ Assets/Scripts/UIScripts/RoleSelectUIManager.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-public class JoinGameUIManager1 : MonoBehaviour
+public class RoleSelectUIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public TextMeshProUGUI header;
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeHeaderText(string text)
     {
-        
+        header.text = text;
     }
 }
