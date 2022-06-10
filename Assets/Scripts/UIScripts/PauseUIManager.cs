dd09286934ff56e2f10f8ff646fd3be44e91a671
Aanastasia Hellemons <amchelle@avans.nl>
2022-06-10 15:45:38 +0100

    [Added] Resume button and hook

--- Assets/Scripts/UIScripts/PauseUIManager.cs
+++ Assets/Scripts/UIScripts/PauseUIManager.cs
@@ -2,13 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseUIManager : MonoBehaviour
 {
     public TextMeshProUGUI header;
 
-    public void changeHeaderText(string text)
+    public TextMeshProUGUI resume;
+
+    public void ChangeHeaderText(string text)
     {
         header.text = text;
+    }
+
+    public void ChangeResumeText(string text)
+    {
+        resume.text = text;
     }
 }
