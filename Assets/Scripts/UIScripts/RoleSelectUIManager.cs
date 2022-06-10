a2f02f98ba9031951767f9d3da85e972a23280f8
Aanastasia Hellemons <amchelle@avans.nl>
2022-06-10 14:04:53 +0100

    [Added] Customer select button and hook

--- Assets/Scripts/UIScripts/RoleSelectUIManager.cs
+++ Assets/Scripts/UIScripts/RoleSelectUIManager.cs
@@ -2,13 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RoleSelectUIManager : MonoBehaviour
 {
     public TextMeshProUGUI header;
 
+    public TextMeshProUGUI customer;
+    public Button customerButton; 
+
     public void ChangeHeaderText(string text)
     {
         header.text = text;
+    }
+
+    public void ChangeCustomerText(string text)
+    {
+        customer.text = text;
+    }
+
+    public void DisableCustomer()
+    {
+        customerButton.gameObject.SetActive(false);
+    }
+
+    public void SelectRole(string role)
+    {
+        Debug.Log($"{role} Selected");
     }
 }
