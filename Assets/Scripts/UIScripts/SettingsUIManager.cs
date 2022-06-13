dc1881eb2781437c1538723f5cbbc2bf93f01298
Aanastasia Hellemons <amchelle@avans.nl>
2022-06-13 09:40:01 +0100

    [Added] Settings Header with hook

--- Assets/Scripts/UIScripts/SettingsUIManager.cs
+++ Assets/Scripts/UIScripts/SettingsUIManager.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class SettingsUIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public TextMeshProUGUI header;
 
-    // Update is called once per frame
-    void Update()
+    public void SetHeaderText(string text)
     {
-        
+        header.text = text;
     }
 }
